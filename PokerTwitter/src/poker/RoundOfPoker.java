e81297ecc4963d5db2d16354786f0e4f75557ce4
14720511 <mantas.ciutys@ucdconnect.ie>
2017-04-28 12:37:35 +0100

    RoundOfPoker almost implemented.

--- PokerTwitter/src/poker/RoundOfPoker.java
+++ PokerTwitter/src/poker/RoundOfPoker.java
@@ -1,6 +1,7 @@
 package poker;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RoundOfPoker {
 
@@ -11,6 +12,8 @@ public class RoundOfPoker {
 	private DeckOfCards deck;
 	private ArrayList<PokerPlayer> originalPlayers; // just in case round has to
 	// be restarted
+	private HashMap<PokerPlayer, Integer> lastBets; // storing last bets of
+	// every player
 
 	public RoundOfPoker(ArrayList<PokerPlayer> players) {
 		createNewRound(players);
@@ -19,8 +22,16 @@ public class RoundOfPoker {
 	private void createNewRound(ArrayList<PokerPlayer> players) {
 		pot = 0;
 		this.players = players;
-		copyOfPlayers(players);
+//		copyOfPlayers(players);
+		originalPlayers = players;
 		deck = new DeckOfCards();
+		lastBets = new HashMap<PokerPlayer, Integer>();
+	}
+
+	private void createHashmap(ArrayList<PokerPlayer> players) {
+		for (int i = 0; i < players.size(); i++) {
+			lastBets.put(players.get(i), 0);
+		}
 	}
 
 	private void copyOfPlayers(ArrayList<PokerPlayer> players) {
@@ -31,33 +42,44 @@ public class RoundOfPoker {
 
 	// Finding out who has won the round
 	private void findWinner() {
-		ArrayList<Integer> winners = new ArrayList<Integer>(); // indexes of
-		// players who
-		// won
-		winners.add(0);
+		System.out.println("------------------");
+		ArrayList<PokerPlayer> winners = new ArrayList<PokerPlayer>();
+
+		winners.add(players.get(0));
+		int currentWinnersGameValue = winners.get(0).getHand().getGameValue();
 		for (int i = 1; i < players.size(); i++) {
-			int currentWinnersGameValue = players.get(winners.get(0)).getHand().getGameValue();
-			int playersGameValue = players.get(i).getHand().getGameValue();
+			PokerPlayer player = players.get(i);
+			int playersGameValue = player.getHand().getGameValue();
 
 			// Comparing game values to find out who has won
 			// If players game value larger than current leading player's one,
 			// replace it
 			if (currentWinnersGameValue < playersGameValue) {
 				winners.clear();
-				winners.add(i);
+				winners.add(player);
+				currentWinnersGameValue = playersGameValue;
 				// if equal, add it to the winners list
 			} else if (currentWinnersGameValue == playersGameValue) {
-				winners.add(i);
+				winners.add(player);
 			}
+		}
+
+		// For testing
+		System.out.println("---------------------");
+		System.out.println("Winners: ");
+		for (int i = 0; i < winners.size(); i++) {
+			System.out.println(winners.get(i).getName() + " ");
 		}
 		splitPot(winners);
 	}
 
 	// Split the pot
-	private void splitPot(ArrayList<Integer> winners) {
+	private void splitPot(ArrayList<PokerPlayer> winners) {
+		System.out.println("------------------");
 		for (int i = 0; i < winners.size(); i++) {
 			int winningPot = pot / winners.size();
-			players.get(i).addToFunds(winningPot);
+			winners.get(i).addToFunds(winningPot);
+			System.out.println(winners.get(i).getName() + " wins " + winningPot);
 		}
 	}
 
@@ -71,38 +93,52 @@ public class RoundOfPoker {
 
 	// Showing amount of chips each player has
 	private void showChips() {
+		System.out.println("------------------");
 		for (int i = 0; i < players.size(); i++) {
-			System.out.println(players.get(i).getFunds());
+			System.out.println(players.get(i).getName() + " has " + players.get(i).getFunds());
 		}
 	}
 
 	// Dealing cards for players
 	private void dealCards() {
+		System.out.println("------------------");
 		for (int i = 0; i < players.size() * HandOfCards.HAND_SIZE; i++) {
 			players.get(i % players.size()).addCard(deck.dealNext());
+		}
+
+		// For testing
+		for (int i = 0; i < players.size(); i++) {
+			System.out.println(players.get(i).getName() + " has: ");
+			System.out.println(players.get(i).getHand());
 		}
 	}
 
 	// Looking for someone who can open
 	private int openingPlayer() {
+		System.out.println("------------------");
 		int openingPlayer = -1;
-		for (int i = players.size() - 1; i >= 0; i--) {
+		int min = players.size();
+
+		for (int i = 0; i < players.size(); i++) {
 			if (players.get(i).canStartBetting()) {
-				openingPlayer = i;
+				if (i <= min) {
+					min = i;
+					openingPlayer = i;
+				}
 				System.out.println(players.get(i).getName() + " can open");
 			} else {
 				System.out.println(players.get(i).getName() + " cannot open");
 			}
 		}
+
 		return openingPlayer;
 	}
 
 	// Discarding cards
 	private void discardCards() {
-		String discarded = "Discarded cards: ";
+		System.out.println("------------------");
 		for (int i = 0; i < players.size(); i++) {
-			discarded += players.get(i).discard();
-			System.out.println(discarded);
+			System.out.println(players.get(i).getName() + " discarded " + players.get(i).discard());
 		}
 	}
 
@@ -112,30 +148,55 @@ public class RoundOfPoker {
 		int lastBet = bet;
 		int counter = players.size(); // when 0, betting is finished
 		for (int i = startingPlayer; counter != 0; i++) {
+			int size = players.size();
+			PokerPlayer player = players.get(i % size);
+			System.out.println("---------------------------------");
+			System.out.println("Index: " + i % players.size());
+			System.out.println("Players to loop through: " + counter);
+			System.out.println("Last bet was " + lastBet);
+			System.out.println("Last bet for " + player.getName() + ": "
+					+ lastBets.get(player));
 			// Get the bet from a player
-			bet = players.get(i % players.size()).bet(bet);
+			System.out.println(player.getName() + " has to add "
+					+ (lastBet - lastBets.get(player)));
+			bet = player.bet((lastBet - lastBets.get(player)));
 
 			// if bet is -1, player folded
 			if (bet == PokerPlayer.BET_FOLD) {
-				System.out.println(players.get(i).getName() + " has folded");
+				System.out.println(player.getName() + " has folded");
 				players.remove(i % players.size());
+				System.out.println("i: " + i % players.size());
+				if (i % players.size() == players.size()) {
+					i = -1;
+				} else {
+					i = (i % players.size()) - 1;					
+				}
+				System.out.println("i: " + i);
 				counter--;
 				// otherwise, add bet to the pot
 			} else {
 				pot += bet;
+				System.out.println(player.getName() + " has bet " + bet);
 				counter--;
+				System.out.println("Diff between lastBet and bet: "
+						+ (bet - (lastBet - lastBets.get(player))));
 				// if new bet higher than last one, player has raised
-				// have to go full circle again
-				if (bet > lastBet) {
-					counter = players.size();
-					lastBet = bet;
+				if (bet > (lastBet - lastBets.get(player))) {
+					System.out.println(player.getName() + " has raised by "
+							+ (bet - (lastBet - lastBets.get(player))));
+					counter = players.size() - 1;
 				}
+				bet += lastBets.get(player);
+				lastBet = bet;
+				lastBets.put(player, bet);
+				System.out.println("Next has to bet in total: " + bet);
 			}
 		}
 	}
 
 	// Play the round
 	public void playRound() {
+		createHashmap(players);
 		showChips();
 		dealCards();
 		int openingPlayer = openingPlayer();
@@ -146,9 +207,11 @@ public class RoundOfPoker {
 			betting(openingPlayer);
 			findWinner();
 			displayCards();
+			showChips();
 			// no one can open, so start new round
 		} else {
-			createNewRound(originalPlayers);
+			System.out.println(players.size());
+			createNewRound(players);
 		}
 	}
 }
